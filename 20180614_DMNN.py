c23478a6e7295e7df4a4e0ae00329baa14ad7a21
sasakishun <Pakka-xeno@keio.jp>
2018-06-20 15:57:32 +0900

    20180620

--- 20180614_DMNN.py
+++ 20180614_DMNN.py
@@ -202,7 +202,6 @@ if __name__ == '__main__':
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
 
-
     y_train = keras.utils.to_categorical(y_train, 10)
     y_test = keras.utils.to_categorical(y_test, 10)
 
